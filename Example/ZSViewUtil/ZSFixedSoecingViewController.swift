71bebdd736c912ef614aeff2328fa4d7811a6493
zhangsen <joshzhang@yntengyun.com>
2020-08-31 11:15:14 +0800

    增加CollectionView等距离显示

---
+++ Example/ZSViewUtil/ZSFixedSoecingViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ZSFixedSoecingViewController.swift
+//  ZSViewUtil_Example
+//
+//  Created by Josh on 2020/8/31.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import ZSViewUtil
+
+class ZSFixedSoecingViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    public lazy var collectionView: ZSCollectionView = {
+        
+        let layout = ZSFixedSpecingFlowLayout(with: .Right)
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 10
+        
+        let collectionView = ZSCollectionView.init(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.white
+        collectionView.allowsSelection = true
+        collectionView.alwaysBounceVertical = true
+        collectionView.alwaysBounceHorizontal = false
+        collectionView.shouldMultipleGestureRecognize = true
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: UICollectionViewCell.identifier)
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: NSStringFromClass(UICollectionReusableView.self) + "Footer")
+        collectionView.register(UICollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: NSStringFromClass(UICollectionReusableView.self) + "Header")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        if #available(iOS 11.0, *) {
+            collectionView.contentInsetAdjustmentBehavior = .never
+        } else {
+            // Fallback on earlier versions
+        }
+        view.addSubview(collectionView)
+        return collectionView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        collectionView.frame = CGRect(x: 0, y: 64, width: 300, height: 31)
+    }
+    
+    // TODO: UICollectionViewDataSource
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: NSStringFromClass(UICollectionViewCell.self), for: indexPath)
+        
+        cell.contentView.backgroundColor = UIColor.brown
+        
+        return cell
+    }
+    
+    open func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        if kind == UICollectionView.elementKindSectionFooter {
+            let footer = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: NSStringFromClass(UICollectionReusableView.self) + "Footer", for: indexPath)
+            footer.backgroundColor = .systemGray
+            return footer
+        }
+        
+        if kind == UICollectionView.elementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: NSStringFromClass(UICollectionReusableView.self) + "Header", for: indexPath)
+            header.backgroundColor = .systemBlue
+            return header
+        }
+        
+        return UICollectionReusableView()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 50 + indexPath.item * 10, height: 30)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        return CGSize(width: collectionView.zs_width, height: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        
+        return CGSize(width: collectionView.zs_width, height: 44.zs_px)
+    }
+}
---
+++ ZSViewUtil/Classes/CollectionView/FlowLayout/ZSFixedSpecingFlowLayout.swift
@@ -0,0 +1,98 @@
+//
+//  ZSFixedSpecingFlowLayout.swift
+//  Kingfisher
+//
+//  Created by Josh on 2020/8/28.
+//
+
+import UIKit
+
+@objc public enum ZSFixedSpecingAlignment: Int {
+    
+    case Left = 0, Right = 2
+}
+
+@objcMembers open class ZSFixedSpecingFlowLayout: UICollectionViewFlowLayout {
+    
+    private override init() {
+        
+        super.init()
+    }
+    
+    private var _alignment_: ZSFixedSpecingAlignment = .Left
+    
+    convenience public init(with alignment: ZSFixedSpecingAlignment) {
+        
+        self.init()
+        _alignment_ = alignment
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    open override var scrollDirection: UICollectionView.ScrollDirection {
+        
+        set { }
+        
+        get {
+           return .vertical
+        }
+    }
+    
+    /**
+     * 这个方法的返回值是一个数组（数组里面存放着rect范围内所有元素的布局属性）
+     * 这个方法的返回值决定了rect范围内所有元素的排布（frame）
+     */
+    open override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
+        // 获得super已经计算好的布局的属性
+        let origins = super.layoutAttributesForElements(in: rect) ?? []
+        let attributes: [UICollectionViewLayoutAttributes] = origins.map({$0.copy() as! UICollectionViewLayoutAttributes})
+        
+        // 计算collectionView最中心点的值
+        for (index, attribute) in attributes.enumerated()
+        {
+            if attribute.representedElementCategory != .cell { continue }
+            
+            let pre = index > 0 ? attributes[index - 1] : nil
+            
+            switch _alignment_
+            {
+            case .Left:
+                if pre?.frame.minY == attribute.frame.minY
+                {
+                    attribute.frame.origin.x = pre!.frame.maxX + minimumLineSpacing
+                }
+                else
+                {
+                    attribute.frame.origin.x = sectionInset.left
+                }
+                break
+            case .Right:
+                
+                if pre?.frame.minY == attribute.frame.minY
+                {
+                    attribute.frame.origin.x = pre!.frame.minX - attribute.frame.size.width - minimumLineSpacing
+                }
+                else
+                {
+                    attribute.frame.origin.x = collectionView!.frame.maxX - attribute.frame.size.width - sectionInset.right
+                }
+                break
+            }
+        }
+        
+        return attributes
+    }
+    
+    /**
+     * 当collectionView的显示范围发生改变的时候，是否需要重新刷新布局
+     * 一旦重新刷新布局，就会重新调用下面的方法：
+     * 1.prepareLayout
+     * 2.layoutAttributesForElementsInRect:方法
+     */
+    override open func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+}
